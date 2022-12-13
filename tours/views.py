b7038b7b79e4e0a595171b1f976c22111c151cf4
AndreyLevi <carmanov.andrey2017@yandex.ru>
2022-12-13 19:50:05 +0300

    В vies.py функию написал, а как выводить не понимаю...

--- tours/views.py
+++ tours/views.py
@@ -11,3 +11,9 @@ def get_total_price(request, id_people):
 	json_data = {"id": price}
 	json_data = json.dumps(json_data)
 	return HttpResponse(json_data, content_type="application/json")
+
+def get_profit(request):
+	price = sum(Tour.total_price() for Tour in Tour_order.objects.filter(status='Завершен'))
+	json_data = {"id": price}
+	json_data = json.dumps(json_data)
+	return HttpResponse(json_data, content_type="application/json")
